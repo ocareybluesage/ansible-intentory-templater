1155dc6ee299a597e062fc22d0c4ccd5095a331c
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-01-28 09:10:21 -0800

    use pydantic to deserialize json into python class

--- src/ec2_client.py
+++ src/ec2_client.py
@@ -1,4 +1,7 @@
 from __future__ import annotations
+from typing import Dict, List
+
+from pydantic import BaseModel
 import boto3
 
 
@@ -7,7 +10,7 @@ class Ec2Client:
         self.client = client
 
     def get_instances_by_env(self, env):
-        foo = self.client.describe_instances(
+        response: Dict[str, str] = self.client.describe_instances(
             Filters=[
                 {
                     "Name": "tag:Env",
@@ -17,11 +20,22 @@ class Ec2Client:
                 },
             ],
         )
-
-        print(f"{foo}")
+        response = Ec2ClientResponse(**response)
+        print(f"{response}")
 
     @staticmethod
     def new() -> Ec2Client:
         client = boto3.client("ec2")
         ec2_client = Ec2Client(client)
         return ec2_client
+
+
+class Ec2ClientResponse(BaseModel):
+    Reservations: List[Reservation]
+
+class Reservation(BaseModel):
+    Instances: List[Instance]
+
+class Instance(BaseModel):
+    PrivateIpAddress: str
+    Tags: List[Dict[str, str]]
