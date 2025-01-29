a8969fef3e6fe454f8036ad51461179d01d89f8a
Omar Carey <omarcarey@Omars-MacBook-Pro.local>
2025-01-29 07:46:17 -0800

    deserialize password and hostname into python class

--- src/ssm_client.py
+++ src/ssm_client.py
@@ -10,32 +10,40 @@ class SsmClient:
     def __init__(self, ssm_client):
         self.ssm_client = ssm_client
 
-    def get_parameter_by_name(self, name):
-        response: Dict[str, str] = self.ssm_client.get_parameters(
-            Names=[
-                f"{name}",
-            ],
-        )        
-        response: SsmClientResponse = SsmClientResponse(**response)
+    def get_parameter_by_name(self, name: str, model: BaseModel) -> BaseModel:
+        response: Dict[str, str] = self.ssm_client.get_parameter(
+            Name=f"{name}", WithDecryption=True
+        )
 
-        [parameter for parameter in response.Parameters if parameter.is_secure_string()]
-        
+        response: SsmClientResponse = SsmClientResponse(**response)
+        response = model.model_validate_json(response.Parameter.get_value())
+
         return response
 
     @staticmethod
     def new(profile_name: str) -> SsmClient:
         session = Session(profile_name=profile_name)
-        ssm_client = session.client('ssm')    
+        ssm_client = session.client("ssm")
 
         return SsmClient(ssm_client)
 
+
 class SsmClientResponse(BaseModel):
-    Parameters: List[Parameter]
+    Parameter: Parameter
 
 class Parameter(BaseModel):
     Name: str
-    Type: str
     Value: str
 
-    def is_secure_string(self) -> bool:
-        return self.Type == "SecureString"
+    def get_value(self) -> str:
+        return self.Value
+
+class RdsCreds(BaseModel):
+    hostname: str
+    password: str
+
+    def get_hostname(self) -> str:
+        return self.hostname
+    
+    def get_password(self) -> str:
+        return self.password
--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from invoke import task
 from src.ec2_client import Ec2Client, Ec2ClientResponse
-from src.ssm_client import SsmClient, SsmClientResponse
+from src.ssm_client import SsmClient, SsmClientResponse, RdsCreds
 from src.templater import Templater
 
 @task
@@ -9,9 +9,8 @@ def foo(_, env: str, client: str):
     ec2_client: Ec2Client = Ec2Client.new(profile_name="it-devops-bss")
     ssm_client: SsmClient = SsmClient.new(profile_name="it-devops-bss-devops")
 
-    response: Ec2ClientResponse = ec2_client.get_instances_by_env(env = env)
-    ssm_response: SsmClientResponse = ssm_client.get_parameter_by_name(f"/secrets/{client}/{env}/database/application-user")
-    print(ssm_response)
+    ec2_response: Ec2ClientResponse = ec2_client.get_instances_by_env(env = env)
+    rds_creds: RdsCreds = ssm_client.get_parameter_by_name(name=f"/secrets/{client}/{env}/database/application-user", model=RdsCreds)
     
-    template_data = {"env": env, "client": client, "instances": response.get_instances()}
+    template_data = {"env": env, "client": client, "instances": ec2_response.get_instances(), "rds_creds": rds_creds}
     templater.render(template_data=template_data)
